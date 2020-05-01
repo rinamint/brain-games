c275842f5e99083bdfefe361ce3fa56c0f0b41b0
rina <mintnight1@gmail.com>
2020-05-01 17:45:40 +0300

    update

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import printName from '../src/cli.js'
 console.log('Welcome to the Brain Games!');
+const userName = printName;
+console.log(`Hello, ${userName}!`);
---
+++ src/cli.js
@@ -0,0 +1,3 @@
+import readlineSync from 'readline-sync';
+const printName = readlineSync.question('May I have your name?');
+export default printName;
