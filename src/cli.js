f24b803dcb600da06c3cca0a0c0f5828ed3e2fd5
rinamint <61691615+rinamint@users.noreply.github.com>
2020-05-03 16:13:36 +0300

    Delete cli.js

--- src/cli.js
+++
@@ -1,9 +0,0 @@
-import readlineSync from 'readline-sync';
-export const hello = () => {
-  console.log('Welcome to the Brain Games!');
-  const printName = readlineSync.question('May I have your name?');
-  console.log(`Hello, ${printName}!`);
-  return printName;
-};
-export const name = hello(); 
-
